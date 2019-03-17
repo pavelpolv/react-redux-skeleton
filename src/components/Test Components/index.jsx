3fb94b5933164d2162d4be617d9b138a8de03c86
Pavel <p.polyakov@seendex.ru>
2019-03-17 22:20:33 +0400

    подготовка файлов

---
+++ src/components/Test Components/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+import './style.scss'
+
+class TestComponents extends React.Component {
+    static propTypes = {}
+
+    render() {
+        return (
+            <div className="test-components">Content</div>
+        );
+    }
+}
+
+export default TestComponents
